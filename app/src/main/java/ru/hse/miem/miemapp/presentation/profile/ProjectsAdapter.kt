137521e47bebc4ccdcdf856bc86e5fd293da49bf
Andrey Bragin <aabragin@miem.hse.ru>
2021-01-06 01:08:30 +0300

    App: few ui fixes (project item in profile & project screen)

--- app/src/main/java/ru/hse/miem/miemapp/presentation/profile/ProjectsAdapter.kt
+++ app/src/main/java/ru/hse/miem/miemapp/presentation/profile/ProjectsAdapter.kt
@@ -1,8 +1,12 @@
 package ru.hse.miem.miemapp.presentation.profile
 
+import android.text.SpannableStringBuilder
+import android.text.style.TypefaceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.color
+import androidx.core.text.inSpans
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_project.view.*
 import ru.hse.miem.miemapp.R
@@ -28,7 +32,14 @@ class ProjectsAdapter(
         fun bind(project: ProjectBasic, navigateToProject: (Long) -> Unit) = itemView.apply {
             projectNumber.text = project.number.toString()
             projectName.text = project.name
-            projectMembers.text = context.getString(R.string.project_members).format(project.members)
+
+            projectMembers.text = SpannableStringBuilder()
+                .color(resources.getColor(R.color.colorPrimaryDark)) {
+                    inSpans(TypefaceSpan("sans-serif-medium")) { append(context.getString(R.string.project_members)) }
+                }
+                .append(" ")
+                .append(project.members.toString())
+
             setOnClickListener { navigateToProject(project.id) }
         }
     }
