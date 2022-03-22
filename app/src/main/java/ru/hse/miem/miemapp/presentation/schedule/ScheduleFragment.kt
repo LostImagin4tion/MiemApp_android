d572a6dbce692ee327f6f8e309c202b9cbf2e60e
egor danilov <evdanilov_2@miem.hse.ru>
2022-03-23 02:47:53 +0300

    fix: fixed bug with redundant schedule updating and loader icon displaying

--- app/src/main/java/ru/hse/miem/miemapp/presentation/schedule/ScheduleFragment.kt
+++ app/src/main/java/ru/hse/miem/miemapp/presentation/schedule/ScheduleFragment.kt
@@ -89,6 +89,8 @@ class ScheduleFragment: BaseFragment(R.layout.fragment_schedule), ScheduleView, 
 
         scheduleCalendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
 
+            bottomScheduleLoader.visibility = View.VISIBLE
+
             val selectedDate = "$dayOfMonth/${month+1}/$year"
 
             scheduleCalendar.date = SimpleDateFormat("dd/MM/yyyy").parse(selectedDate).time
@@ -117,7 +119,10 @@ class ScheduleFragment: BaseFragment(R.layout.fragment_schedule), ScheduleView, 
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
 
-                if(!scheduleList.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                if(!scheduleList.canScrollVertically(1) &&
+                    newState == RecyclerView.SCROLL_STATE_IDLE &&
+                    bottomScheduleLoader.visibility != View.VISIBLE) {
+
                     bottomScheduleLoader.visibility = View.VISIBLE
 
                     schedulePresenter.onScrolledDown(
@@ -176,6 +181,7 @@ class ScheduleFragment: BaseFragment(R.layout.fragment_schedule), ScheduleView, 
         scheduleAdapter.update(items)
 
         scheduleLoader.visibility = View.GONE
+        bottomScheduleLoader.visibility = View.GONE
         scheduleList.visibility = View.VISIBLE
     }
 }
